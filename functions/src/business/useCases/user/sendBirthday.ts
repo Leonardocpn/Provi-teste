18660dcb2a578103ba170c63cec8fbe72b27f7ec
Leonardocpn <leonardocpn@gmail.com>
2020-03-08 10:52:17 -0300

    feat($sendBirthday) : Adicionado o useCase sendBirthday
    Adicionado o useCase sendBirthday

---
+++ functions/src/business/useCases/user/sendBirthday.ts
@@ -0,0 +1,31 @@
+import { GetUserIdFromTokenGateway } from "../../gateways/auth/autenticationGateway";
+import { SendBirthdayUserGateway } from "../../gateways/user/userGateway";
+import moment from 'moment'
+export class SendBirthdayUserUC {
+    constructor(
+        private getUserFromIdGateway: GetUserIdFromTokenGateway,
+        private sendBirthdayUserGateway: SendBirthdayUserGateway
+    ){}
+
+    async execute(input: SendBirthdayUserUCInput): Promise<SendBirthdayUserUCOutput> {
+        try{
+            const userId: string = this.getUserFromIdGateway.getUserIdFromToken(input.token)
+            const date = moment().format("DD/MM/YYYY HH-mm-ss");
+            await this.sendBirthdayUserGateway.sendBirthday(input.data, userId, date)
+            return {message: "Data de nascimento cadastrada com sucesso"}
+        } catch (err){
+            return {
+                message: err.message
+            }
+        }
+    }
+}
+
+export interface SendBirthdayUserUCInput {
+    token: string
+    data: string
+}
+
+export interface SendBirthdayUserUCOutput {
+    message: string
+}
