821f0cb6767a4f3c5e4f6366e575b2a861e8cf03
Leonardocpn <leonardocpn@gmail.com>
2020-03-08 10:37:18 -0300

    chore($root) : Ajustes para EC2
    Adicionada uma classe abstrata com a configuração para conectar-se a EC2

---
+++ functions/src/data/baseDataBase.ts
@@ -0,0 +1,14 @@
+import knex from 'knex';
+
+export abstract class BaseDataBase {
+    protected connection = knex({
+        client: "mysql",
+        connection: {
+            host: process.env.HOST_DB,
+            user: process.env.USER_DB,
+            password: process.env.PASSWORD_DB,
+            database: process.env.DATABASE_DB
+        }
+    })
+
+}
