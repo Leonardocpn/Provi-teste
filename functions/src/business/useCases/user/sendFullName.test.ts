2a3de920a78029c8df3af72569ff3ba9ffdc1008
Leonardocpn <leonardocpn@gmail.com>
2020-03-11 17:09:42 -0300

    test($sendFullName) : Adicionado teste para o useCase sendFullName
    Adicionada uma suíte de teste com para o useCase com um teste
    para validar o envio de um nome d usuario

---
+++ functions/src/business/useCases/user/sendFullName.test.ts
@@ -0,0 +1,37 @@
+import { SendFullNameUserUCInput, SendFullNameUserUC } from "./sendFullName"
+import { SendFullNameUserGateway, GetEndpointsOrder } from "../../gateways/user/userGateway"
+import { GetUserIdFromTokenGateway } from "../../gateways/auth/autenticationGateway"
+
+describe("Test for sendFullNameUC", () => {
+    it("Should save a Full Name for a user",  async () => {
+        const input: SendFullNameUserUCInput = {
+            data:"Antonio Carlos Nascimento",
+            token: "token",
+        }
+
+        const getUserIdFromTokenGateway: GetUserIdFromTokenGateway = {
+            getUserIdFromToken: jest.fn().mockReturnValue("123456789")
+        }
+
+        const sendFullNameGateway: SendFullNameUserGateway = {
+            sendFullNameUser: jest.fn()
+        }
+
+        const getEndpointsOrder: GetEndpointsOrder = {
+            getOrder: jest.fn().mockReturnValue({ order: ('sendCpf,sendFullName,sendBirthday,sendPhoneNumber') })
+        }
+
+        const useCase = new SendFullNameUserUC (
+            getUserIdFromTokenGateway, sendFullNameGateway, getEndpointsOrder, "sendFullName"
+        )
+        
+        const result = await useCase.execute(input)
+
+        expect(result).toEqual({
+            sucess: "true",
+            nextEndpoint: "sendBirthday"
+        })
+        
+    })
+
+})
