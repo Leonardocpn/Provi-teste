82eafdad6d5b0f7a0359c722b0c0d86851f03193
Leonardocpn <leonardocpn@gmail.com>
2020-03-11 17:07:07 -0300

    test($createUser) : Modificado o teste para o useCase createUSer
     Modificado o teste para o useCase createUSer

--- functions/src/business/useCases/user/createUser.test.ts
+++ functions/src/business/useCases/user/createUser.test.ts
@@ -1,41 +1,41 @@
-// import { CreateUserUCInput, CreateUserUC } from "./createUser"
-// import { IdGeneratorGateway } from "../../gateways/services/idGeneratorGateway"
-// import { EncryptCryptographyGateway } from "../../gateways/cryptography/cryptographyGateway"
-// import { GenerateTokenAuthenticationGateway } from "../../gateways/auth/autenticationGateway"
-// import { CreateUserGateway } from "../../gateways/user/userGateway"
-// import { User } from "../../entities/user/user"
+import { CreateUserUCInput, CreateUserUC } from "./createUser"
+import { IdGeneratorGateway } from "../../gateways/services/idGeneratorGateway"
+import { EncryptCryptographyGateway } from "../../gateways/services/cryptographyGateway"
+import { GenerateTokenAuthenticationGateway } from "../../gateways/auth/autenticationGateway"
+import { CreateUserGateway } from "../../gateways/user/userGateway"
+import { User } from "../../entities/user/user"
 
-// describe("Test for createUserUC", () => {
-//     it("Should create a user",  async () => {
-//         const input: CreateUserUCInput = {
-//             email:"leonardo@gmail.com",
-//             password: "1234567",
-//         }
+describe("Test for createUserUC", () => {
+    it("Should create a user",  async () => {
+        const input: CreateUserUCInput = {
+            email:"leonardo@gmail.com",
+            password: "1234567",
+        }
 
-//         const idGeneratorGateway: IdGeneratorGateway ={
-//             generate: jest.fn().mockReturnValue("randomId")
-//         }
+        const idGeneratorGateway: IdGeneratorGateway ={
+            generate: jest.fn().mockReturnValue("randomId")
+        }
 
-//         const cryptographyGateway: EncryptCryptographyGateway = {
-//             encrypt: jest.fn().mockReturnValue("encryptedPassword")
-//         }
+        const cryptographyGateway: EncryptCryptographyGateway = {
+            encrypt: jest.fn().mockReturnValue("encryptedPassword")
+        }
 
-//         const authenticationGateway: GenerateTokenAuthenticationGateway ={
-//             generateToken: jest.fn().mockReturnValue("token")
-//         }
+        const authenticationGateway: GenerateTokenAuthenticationGateway ={
+            generateToken: jest.fn().mockReturnValue("token")
+        }
 
-//         const userGateway: CreateUserGateway = {
-//             createUser: jest.fn().mockReturnValue(new User("randomId", input.email, "encryptedPassword"))
-//         }
+        const userGateway: CreateUserGateway = {
+            createUser: jest.fn().mockReturnValue(new User("randomId", input.email, "encryptedPassword"))
+        }
 
-//         const useCase = new CreateUserUC (
-//             userGateway, cryptographyGateway, idGeneratorGateway, authenticationGateway
-//         )
+        const useCase = new CreateUserUC (
+            userGateway, cryptographyGateway, idGeneratorGateway, authenticationGateway
+        )
         
-//         const result = await useCase.execute(input)
+        const result = await useCase.execute(input)
 
-//         expect(result).toEqual({token: "token"})
+        expect(result).toEqual({token: "token"})
         
-//     })
+    })
 
-// })
+})
